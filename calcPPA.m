5c8174712427bf2cc51ce987cb1e064f046191ed
Nathan Wichers <bluroseai@hotmail.com>
2020-04-01 10:53:17 -0400

    calcFRider and calcPPA
    two new functions to calculate the force of the rider and the positive power average when the rider is doing something

---
+++ calcPPA.m
@@ -0,0 +1,16 @@
+function[PPA] = calcPPA(Frider, vel)
+
+countabovezero = 0;
+PPA = 0;
+for i = 1:length(Frider)
+    Prider(i) = Frider(i) * vel(i);
+    
+    if Prider(i) > 0
+        countabovezero = countabovezero + 1;
+        PPA = PPA + Prider(i);
+    end
+end
+
+PPA = PPA / countabovezero;
+
+end
